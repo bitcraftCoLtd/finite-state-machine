1ac415ee0f62ca0b8180451b74759fbf3d62db8e
Sebastien ROBERT <sebastien.saigo@gmail.com>
2016-11-21 19:49:16 +0900

    Updated AssemblyInfo.cs file

--- Tools/Bitcraft.StateMachineTool/Properties/AssemblyInfo.cs
+++ Tools/Bitcraft.StateMachineTool/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -10,17 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("bitcraft Corporation")]
 [assembly: AssemblyProduct("Bitcraft.StateMachineTool")]
-[assembly: AssemblyCopyright("Copyright © bitcraft Corporation 2014 - 2015")]
+[assembly: AssemblyCopyright("Copyright © bitcraft Corporation 2014 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae4e84e0-236a-4d20-a6c2-c05801cd4e53")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,4 +22,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
